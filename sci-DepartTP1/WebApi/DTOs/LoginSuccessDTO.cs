110dc94aba93e50fdc2e5b631705e50db2910bf5
Mathieu Chaîné <2174193@cegepmontpetit.ca>
2024-09-17 13:07:26 -0400

    FCT : Ajout de AccountController et des DTOs

---
+++ sci-DepartTP1/WebApi/DTOs/LoginSuccessDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.DTOs
+{
+    public class LoginSuccessDTO
+    {
+        [Required]
+        public string Token { get; set; } = "";
+    }
+}
