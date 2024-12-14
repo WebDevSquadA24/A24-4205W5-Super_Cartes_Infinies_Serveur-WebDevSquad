9ecc8fa317ffa0bf4b73181eff5d940b86ec1354
Ramirez <1651537@cegepmontpetit.ca>
2024-12-14 17:36:40 -0500

    PROGRESS: Ajout SendMessage()
    Ceci est un exemple de base pour envoyer un message

--- sci-DepartTP1/WebApi/Hubs/MatchHub.cs
+++ sci-DepartTP1/WebApi/Hubs/MatchHub.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
 using Super_Cartes_Infinies.Combat;
 using Super_Cartes_Infinies.Data;
 using Super_Cartes_Infinies.Models;
@@ -26,46 +27,38 @@ public class MatchHub : Hub
         _playerService = playersService;
         _waitingUserService = waitingUserService;
     }
+    
     public IdentityUser CurentUser
     {
         get
         {
             string userid = Context.UserIdentifier!;
-
             var user = _context.Users.Single(u => u.Id == userid);
 
             return user;
         }
-
     }
+    
     public static class UserHandler
     {
         public static Dictionary<string, string> UserConnections { get; set; } = new Dictionary<string, string>();
     }
-
-
+    
     public override async Task OnConnectedAsync()
     {
-
         await base.OnConnectedAsync();
-
-
     }
-
     
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
         base.OnDisconnectedAsync(exception);
+
         // TODO: Ajouter votre logique
         await _service.StopJoiningMatch(CurentUser.Id);
-        
     }
-
-
+    
     public async Task Connection()
     {
-        
-
         string userId = CurentUser.Id;
         //UserHandler.ConnectedIds.Add(Context.ConnectionId);
         var connectedIdPlayerA = Context.ConnectionId;
@@ -79,12 +72,8 @@ public class MatchHub : Hub
             //UserHandler.ConnectedIds.Add(connectedIdPlayerB);
             if (joiningMatchData.IsStarted)
             {
-
                 //Celui qui appelle la métohde
                 await Clients.Caller.SendAsync("JoiningMatchData", joiningMatchData);
-                
-
-
             }
             else
             {
@@ -93,8 +82,6 @@ public class MatchHub : Hub
 
                 // Il faut tout de même envoyer le joiningMatchData au 2 joueurs
                 await this.StartMatch(userId, joiningMatchData, groupName);
-               
-
             }
         }
         else
@@ -103,39 +90,29 @@ public class MatchHub : Hub
         }
 
     }
+
     public async Task StartMatch(string userId, JoiningMatchData joiningMatchData, string groupName)
     {
-       
-
         //Envoyer à Player A et B
         await Clients.Group(groupName).SendAsync("JoiningMatchData", joiningMatchData);
         StartMatchEvent startMatchEvent = await _service.StartMatch(userId, joiningMatchData.Match);
-
-
         await Clients.Group(groupName).SendAsync("StartMatchEvent", startMatchEvent);
-
-
     }
 
     public async Task EndTurn(JoiningMatchData joiningMatchData)
     {
         string userId = CurentUser.Id;
-
         PlayerEndTurnEvent endTurn = await _service.EndTurn(userId, joiningMatchData.Match.Id);
 
         //current player
         Player player = _playerService.GetPlayerFromUserId(userId);
-
         string groupName = "Match" + joiningMatchData.Match.Id;
-
         await Clients.Group(groupName).SendAsync("EndTurn", endTurn);
-
     }
+
     public async Task Surrender( JoiningMatchData joiningMatchData)
     {
-
         string userId = CurentUser.Id;
-
         SurrenderEvent surrenderEvent = await _service.Surrender(userId, joiningMatchData.Match.Id);
 
         Player player = _playerService.GetPlayerFromUserId(userId);
@@ -146,16 +123,14 @@ public class MatchHub : Hub
 
     public async Task PlayCard(JoiningMatchData joiningMatchData, int playableId)
     {
-
         string userId = CurentUser.Id;
-
         PlayCardEvent playEvent = await _service.PlayCard(userId, joiningMatchData.Match.Id, playableId);
-
         string groupName = "Match" + joiningMatchData.Match.Id;
-
         await Clients.Group(groupName).SendAsync("PlayCard", playEvent);
     }
 
-
-
+    public async Task SendMessage(string text)
+    {
+        await Clients.All.SendAsync("Message", text);
+    }
 }
